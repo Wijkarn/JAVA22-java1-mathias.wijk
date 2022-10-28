224a1cef68a75b479834ab315da134ca96e2a297
Wijkarn <89905143+Wijkarn@users.noreply.github.com>
2022-10-28 11:25:35 +0200

    Add files via upload

--- MP1.java
+++ MP1.java
@@ -3,27 +3,18 @@ import java.util.Scanner;
 public class MP1 {
 
 	public static void main(String[] args) {
-//		Scanner myScanner = new Scanner(System.in);
-//
-//		System.out.println("Guess a number between 0 and 100:");
-//		int number = myScanner.nextInt();
-//		myScanner.nextLine();
-
-		int number = 0;
-		int rand = random();
-
-//		myScanner.close();
-
+		
 		while (true) {
-			checkNumber(number, rand);
+			checkNumber();
 		}
 
 	}
 
-	static int checkNumber(int number, int rand) {
+	static void checkNumber() {
+		int number;
 		int count = 1;
 		Scanner myScanner = new Scanner(System.in);
-		rand = random();
+		int rand = random();
 		System.out.println("Guess a number between 0 and 100:");
 		number = myScanner.nextInt();
 		myScanner.nextLine();
@@ -46,9 +37,6 @@ public class MP1 {
 
 		System.out.println("Your guess was right! You had " + count + " guesses.");
 
-//			myScanner.close();
-
-		return rand;
 	}
 
 	static int random() {
@@ -58,7 +46,7 @@ public class MP1 {
 		double randDTen = rand * 100;
 
 		int round = (int) Math.round(randDTen);
-//		System.out.println("Random " + round);
+		System.out.println("Random " + round);
 
 		return round;
 	}
